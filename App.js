c48be81edbd3d3e836dc32017f8877c40a098ac6
lihsur22 <rushilganguli@outlook.com>
2021-02-25 12:42:41 +0530

    done

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import FB from './screens/fb'
+import IN from './screens/in'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer/>
   );
 }
 
+const tabNavigator = createBottomTabNavigator({
+  Facebook : FB,
+  Instagram : IN
+})
+
+const AppContainer = createAppContainer(tabNavigator);
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
---
+++ screens/in.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+export default class IN extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>kilogram, miligram, instagram</Text>
+            </View>
+        )
+    }
+}
